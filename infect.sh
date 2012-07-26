1421beb81cf4b0605f79fb914007c91f039051b9
Christopher Patton <chpatton013@gmail.com>
2012-07-26 07:50:42 -0700

    infect: pull first; link correctly

--- infect.sh
+++ infect.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Update repository
+git stash
+git pull --rebase
+git submodule update
+git stash pop
+
 # Link config files in home folder
 ln -sf ~/dotfiles/sh/.alias ~/.alias
 ln -sf ~/dotfiles/sh/.git-completion.sh ~/.git-completion.sh
@@ -10,6 +16,6 @@ ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/.vim ~/.vim
 
 # Link plugins for vim
-ln -sf ~/dotfiles/vim/vim-pathogen/autoload/pathogen.vim ~/dotfiles/vim/.vim/autoload/pathogen.vim
-ln -sf ~/dotfiles/vim/vim-fugitive ~/dotfiles/vim/.vim/bundle/vim-fugitive
-ln -sf ~/dotfiles/themes/solarized/vim-colors-solarized ~/dotfiles/vim/.vim/bundle/vim-colors-solarized
+ln -sf ~/dotfiles/vim/vim-pathogen/autoload/pathogen.vim ~/dotfiles/vim/.vim/autoload
+ln -sf ~/dotfiles/vim/vim-fugitive ~/dotfiles/vim/.vim/bundle
+ln -sf ~/dotfiles/themes/solarized/vim-colors-solarized ~/dotfiles/vim/.vim/bundle
