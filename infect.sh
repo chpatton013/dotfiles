e80816fe76745cdce1daa0c542d7f91d16e51d16
Christopher Patton <chpatton013@gmail.com>
2012-07-26 07:28:40 -0700

    infection script links all files

---
+++ infect.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Link config files in home folder
+ln -sf ~/dotfiles/sh/.alias ~/.alias
+ln -sf ~/dotfiles/sh/.git-completion.sh ~/.git-completion.sh
+ln -sf ~/dotfiles/sh/.mybashrc ~/.mybashrc
+ln -sf ~/dotfiles/sh/.no_mux_hosts ~/.no_mux_hosts
+ln -sf ~/dotfiles/sh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/.vim ~/.vim
+
+# Link plugins for vim
+ln -sf ~/dotfiles/vim/vim-pathogen/autoload/pathogen.vim ~/dotfiles/vim/.vim/autoload/pathogen.vim
+ln -sf ~/dotfiles/vim/vim-fugitive ~/dotfiles/vim/.vim/bundle/vim-fugitive
+ln -sf ~/dotfiles/themes/solarized/vim-colors-solarized ~/dotfiles/vim/.vim/bundle/vim-colors-solarized
