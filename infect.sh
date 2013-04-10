7a88db32b8b394dec91e27fe1a10375f408c4eee
Christopher Patton <chpatton013@gmail.com>
2013-04-09 23:35:04 -0700

    get rid of bin. no need for it right now

--- infect.sh
+++ infect.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Update repository
+# Update repository.
 git stash &&
 git pull --rebase &&
 git submodule init &&
 git submodule update &&
 git stash pop
 
-# Remove config file links in home folder
+# Remove config file links in home folder.
 rm -f ~/.alias
 rm -f ~/.commonrc
 rm -rf ~/.dircolors
@@ -22,10 +22,7 @@ rm -f ~/.vimrc
 rm -f ~/.xinitrc
 rm -f ~/.zshrc
 
-# Remove additional executable links
-rm -f ~/bin/premake
-
-# Link config files in home folder
+# Link config files in home folder.
 ln -sf ~/dotfiles/sh/alias.sh ~/.alias
 ln -sf ~/dotfiles/sh/commonrc.sh ~/.commonrc
 ln -sf ~/dotfiles/dircolors/solarized/dircolors.256dark ~/.dircolors
@@ -40,8 +37,5 @@ ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/sh/xinitrc.sh ~/.xinitrc
 ln -sf ~/dotfiles/sh/zshrc.zsh ~/.zshrc
 
-# Link additional executable files
-mkdir -p ~/bin
-ln -sf ~/dotfiles/bin/premake4 ~/bin/premake
-
+# Create directory for software projects.
 mkdir -p ~/Code
