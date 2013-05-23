bad3aae8b891f7fff368c4e4fc22697e1f769a04
Christopher Patton <chpatton013@gmail.com>
2013-05-23 00:01:41 -0700

    terminator solarized

--- infect.sh
+++ infect.sh
@@ -10,6 +10,7 @@ git stash pop
 # Remove config file links in home folder.
 rm -f ~/.alias
 rm -f ~/.commonrc
+rm -rf ~/.config/terminator
 rm -rf ~/.dircolors
 rm -f ~/.git-completion.sh
 rm -f ~/.gitconfig
@@ -22,9 +23,14 @@ rm -f ~/.vimrc
 rm -f ~/.xinitrc
 rm -f ~/.zshrc
 
+# Make sure required directories exist.
+mkdir -p ~/Code
+mkdir -p ~/.config
+
 # Link config files in home folder.
 ln -sf ~/dotfiles/sh/alias.sh ~/.alias
 ln -sf ~/dotfiles/sh/commonrc.sh ~/.commonrc
+ln -sf ~/dotfiles/config/terminator ~/.config/terminator
 ln -sf ~/dotfiles/dircolors/solarized/dircolors.256dark ~/.dircolors
 ln -sf ~/dotfiles/sh/git-completion.sh ~/.git-completion.sh
 ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
@@ -36,6 +42,3 @@ ln -sf ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/sh/xinitrc.sh ~/.xinitrc
 ln -sf ~/dotfiles/sh/zshrc.zsh ~/.zshrc
-
-# Create directory for software projects.
-mkdir -p ~/Code
