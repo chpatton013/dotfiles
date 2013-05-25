9fb63cc2585a0dd6fe9940e3bcf84ff1b56ed6f8
Christopher Patton <chpatton013@gmail.com>
2013-05-25 16:58:07 -0700

    i3 config

--- infect.sh
+++ infect.sh
@@ -9,13 +9,14 @@ git stash pop
 
 # Remove config file links in home folder.
 rm -f ~/.alias
+rm -f ~/.bashrc
 rm -f ~/.commonrc
 rm -rf ~/.config/terminator
 rm -rf ~/.dircolors
 rm -f ~/.git-completion.sh
 rm -f ~/.gitconfig
 rm -f ~/.gitignore
-rm -f ~/.bashrc
+rm -rf ~/.i3
 rm -rf ~/.themes
 rm -f ~/.tmux.conf
 rm -rf ~/.vim
@@ -29,13 +30,14 @@ mkdir -p ~/.config
 
 # Link config files in home folder.
 ln -sf ~/dotfiles/sh/alias.sh ~/.alias
+ln -sf ~/dotfiles/sh/bashrc.bash ~/.bashrc
 ln -sf ~/dotfiles/sh/commonrc.sh ~/.commonrc
 ln -sf ~/dotfiles/config/terminator ~/.config/terminator
 ln -sf ~/dotfiles/dircolors/solarized/dircolors.256dark ~/.dircolors
 ln -sf ~/dotfiles/sh/git-completion.sh ~/.git-completion.sh
 ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/git/gitignore ~/.gitignore
-ln -sf ~/dotfiles/sh/bashrc.bash ~/.bashrc
+ln -sf ~/dotfiles/config/i3 ~/.i3
 ln -sf ~/dotfiles/sh/themes ~/.themes
 ln -sf ~/dotfiles/config/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim ~/.vim
