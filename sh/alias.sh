26007d6d3d1bdbde0cab20df75a36462bbedb2ec
Christopher Patton <chpatton013@gmail.com>
2013-05-25 14:40:39 -0700

    was not working anyway

--- sh/alias.sh
+++ sh/alias.sh
@@ -1,46 +1,44 @@
 # Expand control characters.
-alias less="less -R $*"
+alias less='less -R'
 # Enabled X Forwarding and compression in X sessions.
-alias ssh="ssh -XYC $*"
+alias ssh='ssh -XYC'
 # Pass enviornment variables onto sudo.
-alias sudo="sudo -E $*"
+alias sudo='sudo -E'
 # Make du human readable.
-alias du="du -h $*"
+alias du='du -h'
 # Speed up dd.
-alias dd="dd bs=8196 $*"
+alias dd='dd bs=8196'
 
 # Add color to several commands.
 alias ls='ls --color'
-alias dir='dir --color'
-alias vdir='vdir --color'
 alias grep='grep --color'
 alias fgrep='fgrep --color'
 alias egrep='egrep --color'
 
 # `ls` displays trailing identifiers ('/' or '*'), color, and non-printables.
-alias l="ls -FGb $*"
-alias ll="l -hl $*"
-alias la="l -a $*"
-alias lal="ll -a $*"
-alias lla="lal $*"
+alias l='ls -FGb'
+alias ll='l -hl'
+alias la='l -a'
+alias lal='ll -a'
+alias lla='lal'
 
 # Command-line shortcuts.
-alias ...="cd .. $*"
-alias :q="exit"
-alias c="clear"
-alias g="git $*"
-alias m="make -j 4 $*"
-alias md="mkdir"
+alias ...='cd ..'
+alias :q='exit'
+alias c='clear'
+alias g='git'
+alias m='make -j 4'
+alias md='mkdir'
 
 # School ssh addresses.
-alias multi="ssh chpatton@multicore.csc.calpoly.edu $*"
-alias unix1="ssh chpatton@unix1.csc.calpoly.edu $*"
-alias unix2="ssh chpatton@unix2.csc.calpoly.edu $*"
-alias unix3="ssh chpatton@unix3.csc.calpoly.edu $*"
-alias unix4="ssh chpatton@unix4.csc.calpoly.edu $*"
+alias multi='ssh chpatton@multicore.csc.calpoly.edu'
+alias unix1='ssh chpatton@unix1.csc.calpoly.edu'
+alias unix2='ssh chpatton@unix2.csc.calpoly.edu'
+alias unix3='ssh chpatton@unix3.csc.calpoly.edu'
+alias unix4='ssh chpatton@unix4.csc.calpoly.edu'
 
 # Work ssh addresses.
-alias cmr="ssh cpatton@cominor.com $*"
-alias lb="live_ssh ifixit.com $*"
-alias db="live_ssh db.ifixit.com $*"
-alias slave="live_ssh slave.ifixit.com $*"
+alias cmr='ssh cpatton@cominor.com'
+alias lb='live_ssh ifixit.com'
+alias db='live_ssh db.ifixit.com'
+alias slave='live_ssh slave.ifixit.com'
