a790877c03cc4470bcb0634067c72195a9df5870
Christopher Patton <chpatton013@gmail.com>
2013-06-05 16:00:28 -0600

    make xinitrc a little more portable

--- sh/xinitrc.sh
+++ sh/xinitrc.sh
@@ -12,6 +12,10 @@ if [ -f ~/.config/X/Xresources.xrdb ]; then
    xrdb -merge ~/.config/X/Xresources.xrdb
 fi
 
+if [ -f ~/.Xresources ]; then
+   xrdb -merge ~/.Xresources
+fi
+
 if [ -f ~/.themes/solarized/xresources ]; then
    xrdb -merge ~/.themes/solarized/xresources
 fi
@@ -23,8 +27,22 @@ if [ -d /etc/X11/xinit/xinitrc.d ]; then
    unset f
 fi
 
+if which xscreensaver &> /dev/null; then
+   xscreensaver -no-splash &
+fi
+
+if which xscetroot &> /dev/null; then
+   xsetroot -cursor_name left_ptr &
+fi
+
+if [ -f ~/.fehbg ]; then
+   sh ~/.fehbg &
+fi
+
 case $1 in
-   i3) exec i3;;
-   lxde) exec startlxde;;
-   *);;
+   i3)      exec i3;;
+   lxde)    exec startlxde;;
+   gnome)   exec gnome-session;;
+   kde)     exec startkde;;
+   *)       exec i3;;
 esac
