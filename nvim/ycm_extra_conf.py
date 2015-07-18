c8a42353e7c3d41a756f46294b2b5670316c4019
Christopher Patton <chpatton013@gmail.com>
2015-07-17 17:40:03 -0700

    YCM: add ros libraries

--- nvim/ycm_extra_conf.py
+++ nvim/ycm_extra_conf.py
@@ -1,32 +1,66 @@
+import itertools
 import os
-import ycm_core
+import rospkg
+rospack = rospkg.RosPack()
 
-flags = [
-   '-Wall',
-   '-Wextra',
-   '-Wno-unused-result',
-   '--pipe',
-   '-std=c++11',
-   '-x', 'c++',
-   '-isystem', '/usr/include',
-   '-isystem', '/usr/local/include',
-   '-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1',
-   '-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include',
-   '-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
-   '-isystem', '/System/Library/Frameworks',
-   '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
-   '-I', '.',
-   '-I', 'include',
-   '-I', '~/include',
-]
+def getDefaultFlags():
+    return [
+        '-Wall',
+        '-Wextra',
+        '-Wno-unused-result',
+        '-Weffc++',
+        '--pipe',
+        '-std=c++11',
+        '-x', 'c++',
+    ]
 
+def getSystemIncludeFlags():
+    return getIncludePaths('-isystem', [
+        '/usr/include',
+        '/usr/local/include',
+        '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1',
+        '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include',
+        '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+        '/System/Library/Frameworks',
+        '/System/Library/Frameworks/Python.framework/Headers',
+    ])
+
+def getRosIncludeFlags():
+    paths = []
+
+    ros_workspace = os.path.expandvars('$ROS_WORKSPACE') + '/devel/include'
+    if os.path.isdir(ros_workspace):
+        paths += [ros_workspace]
+
+    paths += [rospack.get_path(path) + '/include' for path in rospack.list()]
+
+    if os.path.isdir('/opt/ros'):
+        paths += [
+                path + '/include'
+                for path in reversed(os.listdir('/opt/ros'))
+                    if os.path.isdir(path) and os.path.isdir(path + '/include')
+                ]
+
+    return getIncludePaths('-isystem', paths)
+
+def getLocalIncludeFlags():
+    return getIncludePaths('-I', [
+        '.',
+        './include',
+        '~/include',
+    ])
+
+def getIncludePaths(prefix, paths):
+    paths = filter(lambda path: os.path.exists(path), set(paths))
+    return list(itertools.chain.from_iterable(
+     itertools.izip([prefix] * len(paths), paths)))
 
 def IsHeaderFile(filename):
-   extension = os.path.splitext(filename)[1]
-   return extension in ['.hpp', '.hxx', '.hh', '.h', '.inl', '.impl']
+    extension = os.path.splitext(filename)[1]
+    return extension in ['.hpp', '.hxx', '.hh', '.h', '.inl', '.impl']
 
 def FlagsForFile(filename, **kwargs):
-   return {
-      'flags': flags,
-      'do_cache': True
-   }
+    return {
+        'flags': getDefaultFlags() + getSystemIncludeFlags() + getRosIncludeFlags() + getLocalIncludeFlags(),
+        'do_cache': True
+    }
