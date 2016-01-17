1ae7584c00431f4b0cfa94405f4fc607b3ea8652
Christopher Patton <chpatton013@gmail.com>
2016-01-16 20:26:40 -0800

    install vagrant and virtualbox

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -3,26 +3,35 @@
 set -euo pipefail
 
 neovim_ppa="ppa:neovim-ppa/unstable"
+
 google_signing_key="https://dl-ssl.google.com/linux/linux_signing_key.pub"
 chrome_source_entry="deb http://dl.google.com/linux/chrome/deb/ stable main"
-chrome_source_list="/etc/apt/sources.list.d/google-chrome.list"
+
 docker_keyserver="hkp://p80.pool.sks-keyservers.net:80"
 docker_key="58118E89F3A912897C070ADBF76221572C52609D"
-docker_source_list="/etc/apt/sources.list.d/docker.list"
-docker_source_entry="deb https://apt.dockerproject.org/repo ubuntu-vivid main"
+docker_source_entry="deb https://apt.dockerproject.org/repo ubuntu-wily main"
+
+virtualbox_signing_key="https://www.virtualbox.org/download/oracle_vbox.asc"
+virtualbox_source_entry="deb http://download.virtualbox.org/virtualbox/debian wily contrib"
+
 rust_install_url="https://static.rust-lang.org/rustup.sh"
 
-# Neovim PPA
 sudo apt-get install software-properties-common
+
+# Neovim PPA
 sudo add-apt-repository --yes "$neovim_ppa"
 
 # Google chrome repository
 wget --quiet --output-document=- "$google_signing_key" | sudo apt-key add -
-sudo su --command="echo '$chrome_source_entry' > $chrome_source_list"
+sudo apt-add-repository --yes "$chrome_source_entry"
 
 # Docker repository
 sudo apt-key adv --keyserver "$docker_keyserver" --recv-keys "$docker_key"
-sudo su --command="echo '$docker_source_entry' > $docker_source_list"
+sudo apt-add-repository --yes "$docker_source_entry"
+
+# Virtualbox repository
+wget --quiet --output-document=- "$virtualbox_signing_key" | sudo apt-key add -
+sudo apt-add-repository --yes "$virtualbox_source_entry"
 
 # Package installation
 sudo apt-get update
@@ -52,7 +61,9 @@ sudo apt-get install --assume-yes \
    python3-pip \
    tmux \
    tree \
+   vagrant \
    vim \
+   virtualbox-5.0 \
    zsh
 
 # Rust install
