56e07ef54aaec53a1b1213ca29d6b1c69a6dd769
Christopher Patton <chpatton013@gmail.com>
2016-01-16 18:45:34 -0800

    install node, npm, rust, and chrome

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -2,11 +2,24 @@
 
 set -euo pipefail
 
+neovim_ppa="ppa:neovim-ppa/unstable"
+google_signing_key=https://dl-ssl.google.com/linux/linux_signing_key.pub
+chrome_source_url="http://dl.google.com/linux/chrome/deb/"
+chrome_source_entry="deb $chrome_source_url stable main"
+chrome_source_list="/etc/apt/sources.list.d/google-chrome.list"
+rust_install_url="https://static.rust-lang.org/rustup.sh"
+
+# Neovim PPA
+sudo apt-get install software-properties-common
+sudo add-apt-repository --yes "$neovim_ppa"
+
+# Google chrome repository
+wget --quiet --output-document=- "$google_signing_key" | sudo apt-key add -
+sudo sh --command="echo '$chrome_source_entry' >> $chrome_source_list"
+
+# Packages
 sudo apt-get update
 sudo apt-get upgrade --assume-yes
-sudo apt-get install software-properties-common
-sudo add-apt-repository --yes ppa:neovim-ppa/unstable
-sudo apt-get update
 sudo apt-get install --assume-yes \
   clang-3.6 \
   clang-3.7 \
@@ -16,9 +29,13 @@ sudo apt-get install --assume-yes \
   g++ \
   gcc \
   git \
+  google-chrome-stable \
   gnome-tweak-tool \
   make \
   neovim \
+  nodejs \
+  nodejs-legacy \
+  npm \
   python-dev \
   python-pip \
   python3-dev \
@@ -27,3 +44,6 @@ sudo apt-get install --assume-yes \
   tree \
   vim \
   zsh
+
+# Rust
+curl --silent --fail --location="$rust_install_url" | sudo sh
