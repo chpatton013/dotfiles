5d189a882259b020602f6ad4ca1abcaef2d32216
Christopher Patton <chpatton013@gmail.com>
2016-03-18 12:06:36 -0700

    Vim: more env variables

--- sh/variables.sh
+++ sh/variables.sh
@@ -2,7 +2,9 @@ export tmux_dark_theme="$HOME/.themes/solarized/tmux-colors-solarized/tmuxcolors
 export tmux_light_theme="$HOME/.themes/solarized/tmux-colors-solarized/tmuxcolors-light.conf"
 export tmux_default_theme="$tmux_dark_theme"
 
+export vim_background="dark"
 export vim_tab_width=3
+
 export BROWSER='chromium-browser'
 export CLICOLOR=TRUE
 export EDITOR=vim
