4bab8e5678643d68a67c4e3ddf11e0d241b2cb02
Christopher Patton <chpatton013@gmail.com>
2016-03-18 12:06:08 -0700

    Tmux: use env variables

--- sh/variables.sh
+++ sh/variables.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env sh
+export tmux_dark_theme="$HOME/.themes/solarized/tmux-colors-solarized/tmuxcolors-dark.conf"
+export tmux_light_theme="$HOME/.themes/solarized/tmux-colors-solarized/tmuxcolors-light.conf"
+export tmux_default_theme="$tmux_dark_theme"
 
 export vim_tab_width=3
 export BROWSER='chromium-browser'
