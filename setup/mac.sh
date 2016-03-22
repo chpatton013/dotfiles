7578dcba0a46e02d327c50ff857959aa96d1ac59
Christopher Patton <chpatton013@gmail.com>
2016-03-22 11:58:14 -0700

    Setup: replace tabs

--- setup/mac.sh
+++ setup/mac.sh
@@ -89,23 +89,23 @@ function install_packages() {
       zsh
 
    # GNU utilities.
-	brew install binutils
-	brew install coreutils
-	brew install diffutils
-	brew install ed --default-names
-	brew install findutils --with-default-names
-	brew install gawk
-	brew install gnu-indent --with-default-names
-	brew install gnu-sed --with-default-names
-	brew install gnu-tar --with-default-names
-	brew install gnu-which --with-default-names
-	brew install gnutls
-	brew install grep --with-default-names
-	brew install gzip
-	brew install screen
-	brew install watch
-	brew install wdiff --with-gettext
-	brew install wget
+   brew install binutils
+   brew install coreutils
+   brew install diffutils
+   brew install ed --default-names
+   brew install findutils --with-default-names
+   brew install gawk
+   brew install gnu-indent --with-default-names
+   brew install gnu-sed --with-default-names
+   brew install gnu-tar --with-default-names
+   brew install gnu-which --with-default-names
+   brew install gnutls
+   brew install grep --with-default-names
+   brew install gzip
+   brew install screen
+   brew install watch
+   brew install wdiff --with-gettext
+   brew install wget
 
    sudo pip2 install neovim
    sudo pip3 install neovim
