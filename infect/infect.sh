17ddb785093a04a4e35045305a80c76517852fc0
Christopher Patton <chpatton013@gmail.com>
2016-10-20 12:26:33 -0700

    Remove useless variable

--- infect/infect.sh
+++ infect/infect.sh
@@ -4,7 +4,6 @@ set -xeuo pipefail
 
 script_dir="$( (builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) )"
 root_dir="$( (builtin cd "$script_dir" && git rev-parse --show-toplevel) )"
-home="$HOME"
 
 source "$script_dir/utilities.sh"
 
@@ -13,7 +12,7 @@ source "$script_dir/utilities.sh"
 ################################################################################
 
 change_shell "$(which zsh)" "$(user_name)"
-make_directory "$home/projects" "$home/.config" "$home/.tmux"
+make_directory "$HOME/projects" "$HOME/.config" "$HOME/.tmux"
 
 ################################################################################
 # Update repository
@@ -27,43 +26,43 @@ git submodule foreach git pull origin master
 ################################################################################
 
 # Shell
-link "$root_dir/sh/alias.sh" "$home/.alias"
-link "$root_dir/sh/function.sh" "$home/.function"
-link "$root_dir/sh/variables.sh" "$home/.variables"
-link "$root_dir/sh/commonrc.sh" "$home/.commonrc"
-link "$root_dir/sh/bashrc.bash" "$home/.bashrc"
-link "$root_dir/sh/zshrc.zsh" "$home/.zshrc"
-link "$root_dir/dircolors/solarized/dircolors.256dark" "$home/.dircolors"
+link "$root_dir/sh/alias.sh" "$HOME/.alias"
+link "$root_dir/sh/function.sh" "$HOME/.function"
+link "$root_dir/sh/variables.sh" "$HOME/.variables"
+link "$root_dir/sh/commonrc.sh" "$HOME/.commonrc"
+link "$root_dir/sh/bashrc.bash" "$HOME/.bashrc"
+link "$root_dir/sh/zshrc.zsh" "$HOME/.zshrc"
+link "$root_dir/dircolors/solarized/dircolors.256dark" "$HOME/.dircolors"
 
 # Tmux
-link "$root_dir/tmux/tmux.conf" "$home/.tmux.conf"
-link "$root_dir/tmux/plugins" "$home/.tmux/plugins"
+link "$root_dir/tmux/tmux.conf" "$HOME/.tmux.conf"
+link "$root_dir/tmux/plugins" "$HOME/.tmux/plugins"
 
 # Git
-link "$root_dir/sh/git-completion.sh" "$home/.git-completion.sh"
-link "$root_dir/git/gitconfig" "$home/.gitconfig"
-link "$root_dir/git/gitignore" "$home/.gitignore"
+link "$root_dir/sh/git-completion.sh" "$HOME/.git-completion.sh"
+link "$root_dir/git/gitconfig" "$HOME/.gitconfig"
+link "$root_dir/git/gitignore" "$HOME/.gitignore"
 
 # Vim
-link "$root_dir/vim" "$home/.vim"
-link "$root_dir/config/nvim" "$home/.config/nvim"
-link "$root_dir/vim/vimrc" "$home/.vimrc"
-link "$root_dir/vim/vimrc" "$home/.config/nvim/init.vim"
-link "$root_dir/vim/ycm_extra_conf.py" "$home/.ycm_extra_conf.py"
+link "$root_dir/vim" "$HOME/.vim"
+link "$root_dir/config/nvim" "$HOME/.config/nvim"
+link "$root_dir/vim/vimrc" "$HOME/.vimrc"
+link "$root_dir/vim/vimrc" "$HOME/.config/nvim/init.vim"
+link "$root_dir/vim/ycm_extra_conf.py" "$HOME/.ycm_extra_conf.py"
 
 # X
-link "$root_dir/sh/background.sh" "$home/.background"
-link "$root_dir/sh/xinitrc.sh" "$home/.xinitrc"
-link "$root_dir/config/X/xscreensaver" "$home/.xscreensaver"
-link "$root_dir/sh/xsession.sh" "$home/.xsession"
-link "$root_dir/config/i3" "$home/.i3"
-link "$root_dir/config/X" "$home/.config/X"
+link "$root_dir/sh/background.sh" "$HOME/.background"
+link "$root_dir/sh/xinitrc.sh" "$HOME/.xinitrc"
+link "$root_dir/config/X/xscreensaver" "$HOME/.xscreensaver"
+link "$root_dir/sh/xsession.sh" "$HOME/.xsession"
+link "$root_dir/config/i3" "$HOME/.i3"
+link "$root_dir/config/X" "$HOME/.config/X"
 
 # Other
-link "$root_dir/themes" "$home/.themes"
-link "$root_dir/ctags" "$home/.ctags"
-link "$root_dir/clang-format.conf" "$home/.clang-format"
-link "$root_dir/editorconfig" "$home/.editorconfig"
+link "$root_dir/themes" "$HOME/.themes"
+link "$root_dir/ctags" "$HOME/.ctags"
+link "$root_dir/clang-format.conf" "$HOME/.clang-format"
+link "$root_dir/editorconfig" "$HOME/.editorconfig"
 
 ################################################################################
 # Build dependencies
